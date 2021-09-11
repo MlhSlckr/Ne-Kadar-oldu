6dd68374f0a470b078f441a18a75d9fae401f36c
MlhSlckr <selcukermelih@gmail.com>
2021-09-11 14:01:06 +0300

    bug fixed

--- app.js
+++ app.js
@@ -4,40 +4,13 @@ const dakika = document.querySelector('.dakika')
 const saniye = document.querySelector('.saniye')
 const date = document.querySelector('.date')
 const button = document.querySelector('.button')
-/*
-button.addEventListener('click', () => {
-  function countdown() {
-    const tarih = date.value;
-    const tarihDate = new Date(tarih);
-    const currentDate = new Date();
-
-    const totalSeconds = (tarihDate - currentDate) / 1000;
-
-    const days = Math.floor(totalSeconds / 3600 / 24);
-    const hours = Math.floor(totalSeconds / 3600) % 24;
-    const mins = Math.floor(totalSeconds / 60) % 60;
-    const seconds = Math.floor(totalSeconds) % 60;
-
-    gün.textContent = formatTime(days * -1);
-    saat.textContent = formatTime(hours * -1);
-    dakika.textContent = formatTime(mins * -1);
-    saniye.textContent = formatTime(seconds * -1);
-  }
-  function formatTime(time) {
-    return time < 10 ? `0${time}` : time;
-  }
-
-  countdown();s
-  setInterval(countdown, 1000);
-})
-*/
 
 
 button.addEventListener('click', () => {
   const tarih = date.value;
   function countdown() {
-    const tarihDate = new Date(tarih);
-    const currentDate = new Date();
+    const tarihDate = new Date(tarih)
+    const currentDate = new Date()
 
     const totalSeconds = (tarihDate - currentDate) / 1000;
 
@@ -46,24 +19,21 @@ button.addEventListener('click', () => {
     const mins = Math.floor(totalSeconds / 60) % 60;
     const seconds = Math.floor(totalSeconds) % 60;
 
-    gün.textContent = formatTime(days);
-    saat.textContent = formatTime(hours);
-    dakika.textContent = formatTime(mins);
-    saniye.textContent = formatTime(seconds);
+    gün.textContent = formatTimer(days);
+    saat.textContent = formatTimer(hours);
+    dakika.textContent = formatTimer(mins);
+    saniye.textContent = formatTimer(seconds);
     localStorage.setItem('dateValue', date.value)
-
   }
 
-  function formatTime(time) {
+  function formatTimer(time) {
     return time < 10 ? `0${time}` : time;
   }
 
-  countdown();
+  countdown()
 
   setInterval(countdown, 1000)
-
 })
-
 
 if (localStorage.dateValue) {
   tarih = localStorage.dateValue
@@ -84,7 +54,6 @@ if (localStorage.dateValue) {
     saat.textContent = formatTime(hours);
     dakika.textContent = formatTime(mins);
     saniye.textContent = formatTime(seconds);
-
   }
 
   function formatTime(time) {
